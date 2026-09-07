b3d20df74e80e30a41b10d9c01a65d051152d8cb
Jung Geun Hwi <89985632+llAlphaGoll@users.noreply.github.com>
2026-09-07 09:30:48 +0900

    Update carstate.py
    # DELETE

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -62,7 +62,7 @@ class CarState(CarStateBase):
     ret.wheelSpeeds = self.get_wheel_speeds(cp.vl["WHL_SPD11"]["WHL_SPD_FL"], cp.vl["WHL_SPD11"]["WHL_SPD_FR"],
                                             cp.vl["WHL_SPD11"]["WHL_SPD_RL"], cp.vl["WHL_SPD11"]["WHL_SPD_RR"])
 
-   # ret.vEgoRaw = (ret.wheelSpeeds.fl + ret.wheelSpeeds.fr + ret.wheelSpeeds.rl + ret.wheelSpeeds.rr) / 4.
+    ret.vEgoRaw = (ret.wheelSpeeds.fl + ret.wheelSpeeds.fr + ret.wheelSpeeds.rl + ret.wheelSpeeds.rr) / 4.
     
     ret.clu_Vanz = cp.vl["CLU11"]["CF_Clu_Vanz"] * CV.KPH_TO_MS
     
